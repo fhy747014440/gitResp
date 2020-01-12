695eb7ece74cc9285ae04af3ecbfa5d2a6648c05
张三 <zhangsan@gmail>
2020-01-12 14:55:40 +0800

    李四分支提交

--- src/main/java/com/cy/mp/controller/UserController.java
+++ src/main/java/com/cy/mp/controller/UserController.java
@@ -1,12 +1,8 @@
 package com.cy.mp.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 @RestController
---
+++ src/main/java/com/cy/mp/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.cy.mp.entity;
+
+
+public class Employee {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Employee() {
+    }
+
+    public Employee(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
